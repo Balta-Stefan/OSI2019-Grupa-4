43f97b1d9afd3cf88ca867ddd6df904a692b5764
markovranjes5 <58687793+markovranjes5@users.noreply.github.com>
2019-12-22 23:59:11 +0100

    DodanaFunkcijaLogin

--- backEnd.cpp
+++ backEnd.cpp
@@ -1,108 +1,177 @@
 #include "backEnd.h"
 
+static const char alphanum[] =
+"0123456789"
+"!@#$%^&*"
+"ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+"abcdefghijklmnopqrstuvwxyz";
+int stringLength = sizeof(alphanum) - 1;
+
+
+std::vector<struct session> sessions;
+std::string fileOpeningError = "Greska pri otvaranju fajla.",successfulLogin="Uspjesno prijavljivanje.",unsuccessfulLogin="Neuspjesno prijavljivanje.";
+
 bool authenticate(std::string sessionID)
 {
-    //provjeriti da li globalni vektor zvan "sessions", koristiti for(auto &counter : sessions) petlju
-        //sadrzi sessionID pa vratiti odgovarajuci bool
+	//provjeriti da li globalni vektor zvan "sessions", koristiti for(auto &counter : sessions) petlju
+		//sadrzi sessionID pa vratiti odgovarajuci bool
 }
 
 bool isAdmin(std::string sessionID)
 {
-    //isto kao funkcija authenticate, provjeriti da li je proslijedjeni sessionID od administratora
-    //povratna vrijednost je bool i false je za neuspjesno logovanje, true u protivnom
+	//isto kao funkcija authenticate, provjeriti da li je proslijedjeni sessionID od administratora
+	//povratna vrijednost je bool i false je za neuspjesno logovanje, true u protivnom
 }
 
 std::string login(std::string username, std::string password)
 {
-    //provjeriti fajl "korisnici.txt"
-    //ako postoji takav korisnik, napraviti struct session i staviti u globalni vektor sessions
+	std::string fuserID, fusername, fpassword, fnumber, fline;
+	int  flag;
+	std::ifstream users("korisnici.txt");
+	if (users.is_open() == false)
+		return fileOpeningError;
+	while (std::getline(users, fline))
+	{
+		std::stringstream iss(fline);
+		std::getline(iss, fuserID, '-');
+		std::getline(iss, fusername, '-');
+		std::getline(iss, fpassword, '-');
+		std::getline(iss, fnumber, '\n');
+		int fnum = std::stoi(fnumber);
+		if (fusername == username && fpassword == password)
+		{
+			flag = 1;
+			std::string sessionID = "";
+			do
+			{
+				genRandomString(sessionID);
+			} while (checkSessionID(sessionID));
+			struct session newSession;
+			newSession.sessionID = sessionID;
+			newSession.userID = fuserID;
+			newSession.userName = fusername;
+			newSession.userRank = fnum;
+			sessions.push_back(newSession);
+			return successfulLogin;
+		}
+
+	}
+	return unsuccessfulLogin;
+	users.close();
 }
 
 std::string eventComment(std::string comment, std::string eventName)
 {
-    //staviti komentar comment u dogadjaj sa nazivom eventName
-    //voditi racuna o formatu u fajlu komentari.txt
-    //povratna vrijednost je poruka o uspjesnosti, koristiti neku globalnu promjenljivu
+	//staviti komentar comment u dogadjaj sa nazivom eventName
+	//voditi racuna o formatu u fajlu komentari.txt
+	//povratna vrijednost je poruka o uspjesnosti, koristiti neku globalnu promjenljivu
 }
 
 std::string banUser(std::string userName)
 {
-    //u fajlu korisnici.txt pronaci korisnika userName
-    //promijeniti mu zadnji element u redu (broj) u nulu
-    //povratna vrijednost je poruka o uspjesnosti, koristiti neku globalnu promjenljivu
+	//u fajlu korisnici.txt pronaci korisnika userName
+	//promijeniti mu zadnji element u redu (broj) u nulu
+	//povratna vrijednost je poruka o uspjesnosti, koristiti neku globalnu promjenljivu
 }
 
 struct quiz getQuizInfo()
 {
-  //POPUNITI STRUKTURU STRUCT QUIZ
-  //ovo je funkcija koja pribavlja podatke o kvizu za administratora
-  //vratiti tu popunjenu strukturu
+	//POPUNITI STRUKTURU STRUCT QUIZ
+	//ovo je funkcija koja pribavlja podatke o kvizu za administratora
+	//vratiti tu popunjenu strukturu
 }
 
 void editQuiz(struct quiz newQuiz)
 {
-    //FUNKCIJA ZA ADMINISTRATORA, KORISTI SE ZA PROMJENU KVIZA
+	//FUNKCIJA ZA ADMINISTRATORA, KORISTI SE ZA PROMJENU KVIZA
 }
 
 std::vector<std::string> getCategories()
 {
-    //FUNKCIJA KOJA PRIBAVLJA KATEGORIJE ZA ADMINISTRATORA
-    //NALAZE SE U FAJLU kategorije.txt
-    //POPUNITI VEKTOR STRINGOVA I VRATITI GA
+	//FUNKCIJA KOJA PRIBAVLJA KATEGORIJE ZA ADMINISTRATORA
+	//NALAZE SE U FAJLU kategorije.txt
+	//POPUNITI VEKTOR STRINGOVA I VRATITI GA
 }
 
 void changeCategories(std::vector<std::string> newCategories)
 {
-    //ADMINISTRATORKSA FUNKCIJA KOJA MIJENJA KATEGORIJE
-    //SADRZAJ VEKTORA newCategories UPISATI U FAJL kategorije.txt, CITAVOG GA IZMIJENITI
+	//ADMINISTRATORKSA FUNKCIJA KOJA MIJENJA KATEGORIJE
+	//SADRZAJ VEKTORA newCategories UPISATI U FAJL kategorije.txt, CITAVOG GA IZMIJENITI
 }
 
 struct quiz4Players getQuestions4Player()
 {
-    //FUNKCIJA KOJA PRIBAVLJA PITANJA I PONUDJENE ODGOVORE ZA IGRACE
+	//FUNKCIJA KOJA PRIBAVLJA PITANJA I PONUDJENE ODGOVORE ZA IGRACE
 }
 
 int checkPlayersAnswers(struct quiz)
 {
-    //FUNKCIJA KOJA PROVJERAVA KOLIKO JE ODGOVORA ISPRAVNO
-    //VRATITI BROJ ISPRAVNIH ODGOVORA
+	//FUNKCIJA KOJA PROVJERAVA KOLIKO JE ODGOVORA ISPRAVNO
+	//VRATITI BROJ ISPRAVNIH ODGOVORA
 }
 
 std::string removeComment(std::string commentID, std::string eventName)
 {
-    //ADMINISTRATORSKA FUNKCIJA ZA UKLANJANJE KOMENTARA
-    //U FOLDERU eventName SE NALAZI FAJL komentari.txt
-    //UKLONITI KOMENTAR U REDU SA commentID
+	//ADMINISTRATORSKA FUNKCIJA ZA UKLANJANJE KOMENTARA
+	//U FOLDERU eventName SE NALAZI FAJL komentari.txt
+	//UKLONITI KOMENTAR U REDU SA commentID
 }
 
 
 std::vector<eventList> getEvents(struct eventFilter)
-{	
+{
 	/*
 	prihvaca strukturu eventFilter koja daje informacije o tome sta korisnik zeli da trazi, vraca se vektor sa strukturama od kojih svaka daje informacije o nekom dogadjaju
 	*/
 }
 
-std::vector<event> getEvent(std::string eventName)
-{	
+struct event getEvent(std::string eventName)
+{
 	/*
-	prihvata sve informacije o konkretnom dogadjaju 
+	prihvata sve informacije o konkretnom dogadjaju
 	*/
-	
+
 }
 
 
 std::string addEvent(struct newEvent &event2Add)
-{	
+{
 	/*
 	funkcija za dodavanje dogadjaja u bazu podataka
 	provjerava da li dogadjaj vec postoji uporedjivanjem imena i svih ostalih podataka (vrijeme, datum, mjesto i kategorija)
-	ako dogadjaj vec postoji, vratiti odgovarajucu poruku 
-	
+	ako dogadjaj vec postoji, vratiti odgovarajucu poruku
+
 	povratna vrijednost je poruka o uspjesnosti
 	*/
-	
+
+}
+
+char genRandomChar()  // Funkcija za generisanje random stringa.
+{
+
+	return alphanum[rand() % stringLength];
+}
+
+std::string genRandomString(std::string& sessionID)
+{
+	for (int z = 0; z < 32; z++)
+	{
+		sessionID += genRandomChar();
+	}
+	return sessionID;
+}
+
+bool checkSessionID(std::string& sessionID)
+{
+	for (unsigned int i=0; i<sessions.size(); ++i)
+		if (sessionID == sessions[i].sessionID)
+			return true;
+	return false;
 }
 
 
-
+void printSessions()
+{
+	for(unsigned int i=0;i<sessions.size();i++)
+		std::cout << sessions[i].sessionID << " " << sessions[i].userID << " " << sessions[i].userName << " " << sessions[i].userRank << std::endl;
+}
--- backEnd.h
+++ backEnd.h
@@ -2,56 +2,61 @@
 #define BACKEND_H_INCLUDED
 #include <string>
 #include <vector>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <ctime>
+#include <cstdlib>
 
 //global variable: vector<struct session> sessions
 
 struct loginInfo
 {
-    std::string username, password;
+	std::string username, password;
 };
 
 struct session
 {
-    std::string sessionID, userName, userID;
-    char userRank; //0 za obicne korisnike, 1 za administratore
+	std::string sessionID, userName, userID;
+	int userRank; //0 za obicne korisnike, 1 za administratore
 };
 
 struct quiz
 {
-    std::string questions, answers;
-    std::vector<int> rightAnswers;
+	std::string questions, answers;
+	std::vector<int> rightAnswers;
 };
 
 struct quiz4Players
 {
-    std::string questions, answers;
+	std::string questions, answers;
 };
 
-struct eventList 
+struct eventList
 {	//sadrzi osnovne informacije za pregled dogadjaja na glavnoj stranici
 	//slika, ostaviti za kasnije
 	std::string eventName;
 	std::string shortDescription;
 	unsigned short startHour, startMinute, endHour, endMinute, startMonth, startDay, startYear, endMonth, endDay, endYear; //informacije o pocetku i zavrsetu dogadjaja 
 	std::string location; //mjesto odrzavanja dogadjaja 
-	std::vector<std::string>specialRequirements //posebni zahtjevi dogadjaja 
+	std::vector<std::string>specialRequirements; //posebni zahtjevi dogadjaja 
 };
 
 struct eventFilter
 {	//koristi se za filtriranje dogadjaja 
 	unsigned short startHour, startMinute, endHour, endMinute, startMonth, startDay, startYear, endMonth, endDay, endYear; //informacije o pocetku i zavrsetu dogadjaja 
 	std::string location; //mjesto odrzavanja dogadjaj 
-	std::string category; 
+	std::string category;
 };
 
-struct event 
+struct event
 {	//daje informacije o konkretnom dogadjaju
 
 	//dodati sliku kasnije
 	struct eventList data; //sadrzi i samu strukturu eventList
 	std::string description; //tekst koji opisuje dogadjaj
 	std::string category;
-	
+
 	//i-ti indeksi vektora ispod su u vezi
 	std::vector<std::string>userNames; //korisnicka imena 
 	std::vector<std::string>comments; //sadrzi komentare od korisnika iz vektora userNames
@@ -81,6 +86,10 @@ std::string removeComment(std::string commentID, std::string eventName);
 struct event getEvent(std::string eventName);
 std::vector<eventList> getEvents(struct eventFilter &filter);
 std::string addEvent(struct newEvent &event2Add);
+char genRandomChar();
+std::string genRandomString(std::string&);
+bool checkSessionID(std::string&);
+void printSessions();
 
 
 
